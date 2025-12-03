41b9199ec6de3ed6a87f24aef9899b545c1f5c80
Rocky Jaiswal <rocky.jaiswal@gmail.com>
2025-12-03 22:03:43 +0100

    d2 p2

--- py/src/day_3.py
+++ py/src/day_3.py
@@ -1,4 +1,5 @@
 from utils import read_data_file
+# from itertools import combinations
 
 
 class Day3:
@@ -15,6 +16,38 @@ class Day3:
                 pairs.append(int(f"{list_int[i]}{list_int[j]}"))
 
         return max(pairs)
+    
+    def find_largest_by_removing(self, lst: list[str], to_remove: int) -> int:
+        stack = []
+        removals_left = to_remove
+        
+        for digit in lst:
+            while stack and removals_left > 0 and stack[-1] < digit:
+                stack.pop()
+                removals_left -= 1
+            stack.append(digit)
+        
+        if removals_left > 0:
+            stack = stack[:-removals_left]
+        
+        return int(''.join(stack))
+    
+    def omit_at_indexes(self, lst, indexes_to_omit):
+        return int(''.join(str(item) for i, item in enumerate(lst) if i not in indexes_to_omit))
+    
+    def find_largest_num(self, lst: list[str]):
+        length = len(lst)
+
+        # skippables = []
+        # for combo in combinations(range(length), length - 12):
+        #     # print(combo)
+        #     skippables.append(list(combo))
+            
+        # nums = []
+        # for skips in skippables:
+        #     nums.append(self.omit_at_indexes(lst, skips))
+
+        return self.find_largest_by_removing(lst, length - 12)
 
     def part_1(self):
         ratings = read_data_file("day3_2.txt", "\n")
@@ -25,6 +58,16 @@ class Day3:
 
         print(sum(lgst))
 
+    def part_2(self):
+        ratings = read_data_file("day3_2.txt", "\n")
+        lgst = []
+
+        for rating in ratings:
+            lgst.append(self.find_largest_num(list(rating)))
+
+        print(sum(lgst))
+
 
 if __name__ == "__main__":
-    Day3().part_1()
+    # Day3().part_1()
+    Day3().part_2()
